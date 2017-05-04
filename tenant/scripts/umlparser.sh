b52241b845730de5317a281449a0c7fbd247f0a2
Binoy Prakash <prakash.binoy@gmail.com>
2017-05-04 02:08:41 -0700

    Sanitizes parser script

--- tenant/scripts/umlparser.sh
+++ tenant/scripts/umlparser.sh
@@ -32,6 +32,9 @@ if [[ $# -lt 2 ]] ; then
     exit 1
 fi
 
+IP=$1
+OP=${2//\//}
+
 if ! $sequenceflag
 then
   # Check for graphviz
@@ -41,13 +44,13 @@ then
   fi
 
   #Cleanup exisiting .java & .png files
-  [ -e "$2.java" ] && rm "$2.java"
-  [ -e "$2.png" ] && rm "$2.png"
+  [ -e "$OP.java" ] && rm "$OP.java"
+  [ -e "$OP.png" ] && rm "$OP.png"
 
   echo 'Preparing class diagram ...'
-  java -jar "$BASEDIR/uml-class-diagram.jar" $1 $2
-  echo "Drawing $2.png"
-  java -jar "$BASEDIR/umlgraph-5.7.2.23.jar" -private -attributes -operations -types -visibility -output - "$2.java" | dot -Tpng -Gdpi=200 -o"$2.png" 
+  java -jar "$BASEDIR/uml-class-diagram.jar" $IP $OP
+  echo "Drawing $OP.png"
+  java -jar "$BASEDIR/umlgraph-5.7.2.23.jar" -private -attributes -operations -types -visibility -output - "$OP.java" | dot -Tpng -Gdpi=200 -o"$OP.png" 
 else
   # Check for ajc
   if [ -z `which ajc` ]; then
@@ -62,16 +65,16 @@ else
 
   echo 'Preparing sequence diagram ...'
   tmpdir=`mktemp -d`
-  cp "$1/"*".java" $tmpdir
+  cp "$IP/"*".java" $tmpdir
   cp "$BASEDIR/TracingAspect.aj" $tmpdir
   ajc -1.8 $tmpdir/*.java $tmpdir/*.aj
   java -cp "$BASEDIR/aspectjrt-1.8.8.jar:$tmpdir" Main
   cp "$BASEDIR/sequence.pic" .
-  pic2plot -Tsvg  uml.pic > "$2.svg.tmp"
-  awk 'NR==1,/transform/{sub(/transform=".*" xml/, "transform=\"translate(0.25,0.05) scale(0.06, -0.06)\" xml")} 1' "$2.svg.tmp" > "$2.svg"
+  pic2plot -Tsvg  uml.pic > "$OP.svg.tmp"
+  awk 'NR==1,/transform/{sub(/transform=".*" xml/, "transform=\"translate(0.25,0.05) scale(0.06, -0.06)\" xml")} 1' "$OP.svg.tmp" > "$OP.svg"
 
   # Cleaning up files
   rm sequence.pic
   rm -rf $tmpdir
-  rm $2.svg.tmp
+  rm $OP.svg.tmp
 fi
